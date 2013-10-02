c4b8b203eaed8a678e28e66645b7a329fdb9d0ed
Ben Doherty <ben_doherty@bvn.com.au>
2013-10-03 09:53:09 +1000

    versions used in report

--- msaTemplates.py
+++ msaTemplates.py
@@ -15,34 +15,36 @@ headTmpl = Template(u'''\
   ''')
 
 eachimgTempl = Template(u'''\    
-<tr>       
-    
-        {%- for img in row %} 
-        {% if loop.first %} 
-            <td class="row-title">
-                <div class"info-box">
-                    <p class="month">{{monthNames[img.month]}}</p>
-                    <p>level: {{img.building_level}}</p>
-                    <p>Appt: {{img.appartment}}</p>
-                </div>
-            </td>
-        {% endif %}    
-        <td class="render appt-{{img.appartment}} level-{{img.building_level}}" data-percentWhite="{{img.pcWhite}}">
-            <p><img src="{{img.path}}/{{ img.filename }}" title="|{{ '{0:0>2}'.format(img.pcWhite)}}| {{ img.hour }}:{{ '{0:0>2}'.format(img.minute) }} - {{ img.filename }}" /></p>
-            
-            <p class="time">{{ img.hour }}:{{ '{0:0>2}'.format(img.minute) }} {{ '{0:0>2}'.format(img.pcWhite)}}</p>
-        <td>            
-        {% if loop.last %} 
-            <td class="row-title {{window['passStatus']}}">
-                <div class"info-box">
-                    <h1 class="pass-status">{{window['passStatus']}}</h1>
-                    <p>Total Hours: {{window['totalHours']}}</p>
-                    <p>Bracket Hours: {{window['inBracketHours']}}</p>
-                </div>
-            </td>
-        {% endif %}
-        {%- endfor %}
-    
+<tr>    
+    {%- for img in row %} 
+    {% if loop.first %} 
+        <td class="info-box month">
+            {{monthNames[img.month]}}
+        </td>
+        <td class="info-box">
+            level: {{img.building_level}}
+        </td>
+        <td class="info-box">
+                Appt: {{img.appartment}}
+        </td>
+    {% endif %}    
+    <td class="render appt-{{img.appartment}} level-{{img.building_level}}" data-percentWhite="{{img.pcWhite}}">
+        <p><img src="{{img.path}}/{{ img.filename }}" title="|{{ '{0:0>2}'.format(img.pcWhite)}}| {{ img.hour }}:{{ '{0:0>2}'.format(img.minute) }} - {{ img.filename }}" /></p>
+        
+        <span class="time">{{ img.hour }}:{{ '{0:0>2}'.format(img.minute) }}</span> <span class="pc-white">{{ '{0:0>2}'.format(img.pcWhite)}}</span>
+    </td>            
+    {% if loop.last %} 
+        <td class="{{window['passStatus']}} info-box">                
+            <h1 class="pass-status">{{window['passStatus']}}</h1>
+        </td>
+        <td class="info-box">
+            <p>Total Hours: {{window['totalHours']}}</p>
+        </td>
+        <td class="info-box">
+            <p>Bracket Hours: {{window['inBracketHours']}}</p>                
+        </td>
+    {% endif %}
+    {%- endfor %}    
 </tr>    
 ''')
 
