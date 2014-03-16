496a841b9d7e639602c0e66fa199d350aaa77bfc
Ben Doherty <ben_doherty@bvn.com.au>
2014-03-16 14:46:19 +1100

    organising before refactoring

--- msaTemplates.py
+++ msaTemplates.py
@@ -17,33 +17,34 @@ headTmpl = Template(u'''\
 eachimgTempl = Template(u'''\    
 <tr>    
     {%- for img in row %} 
-    {% if loop.first %} 
-        <td class="info-box month">
-            {{monthNames[img.month]}}
-        </td>
-        <td class="info-box">
-            level: {{img.building_level}}
-        </td>
-        <td class="info-box">
-                Appt: {{img.appartment}}
-        </td>
-    {% endif %}    
+        {% if loop.first %} 
+            <td class="info-box month">
+                <!-- {{monthNames[img.month]}} -->
+                level: {{img.name.split("-")[1]}}
+            </td>
+            <td class="info-box">                
+                Appt:  {{img.name.split("-")[2]}}                
+            </td>
+            <td class="info-box">
+                Win:   {{img.name.split("-")[3]}}
+            </td>
+        {% endif %}    
     <td class="render appt-{{img.appartment}} level-{{img.building_level}}" data-percentWhite="{{img.pcWhite}}">
         <p><img src="{{img.path}}/{{ img.filename }}" title="|{{ '{0:0>2}'.format(img.pcWhite)}}| {{ img.hour }}:{{ '{0:0>2}'.format(img.minute) }} - {{ img.filename }}" /></p>
         
         <span class="time">{{ img.hour }}:{{ '{0:0>2}'.format(img.minute) }}</span> <span class="pc-white">{{ '{0:0>2}'.format(img.pcWhite)}}</span>
     </td>            
-    {% if loop.last %} 
-        <td class="{{window['passStatus']}} info-box">                
-            <h1 class="pass-status">{{window['passStatus']}}</h1>
-        </td>
-        <td class="info-box">
-            <p>Total Hours: {{window['totalHours']}}</p>
-        </td>
-        <td class="info-box">
-            <p>Bracket Hours: {{window['inBracketHours']}}</p>                
-        </td>
-    {% endif %}
+        {% if loop.last %} 
+            <td class="{{window['passStatus']}} info-box">                
+                <h1 class="pass-status">{{window['passStatus']}}</h1>
+            </td>
+            <td class="info-box">
+                <p>Total Hours: {{window['totalHours']}}</p>
+            </td>
+            <td class="info-box">
+                <p>Bracket Hours: {{window['inBracketHours']}}</p>                
+            </td>
+        {% endif %}
     {%- endfor %}    
 </tr>    
 ''')
--- msamain.py
+++ msamain.py
@@ -43,7 +43,7 @@ print(path)
 print
 
 numberOfHoursToPass = 2
-numberOfImagesPerHour = 2      
+numberOfImagesPerHour = 4      
 bracketStartTime =  9
 BracketEndTime   = 15  
 print "numberOfHoursToPass", numberOfHoursToPass, "numberOfImagesPerHour", numberOfImagesPerHour, "bracketStartTime", bracketStartTime, "BracketEndTime", BracketEndTime
@@ -53,9 +53,33 @@ print
 saImages = getFileList(path, ('minute','hour','appartment_window',
                               'appartment','building_level','month'))
 
-for building in ["A", "B", "C"]:
+appts={}
+for i in saImages:
+    if i.name in appts:
+        appts[i.name] +=1
+    else:
+        appts[i.name] =1
+#print appts
+
+#explicity name the appts
+buildingA = []
+buildingB = []
+buildingC = []
+
+
+for name in appts.keys():
+    if name[0] == "A":
+        buildingA.append(name)
+    if name[0] == "B":
+        buildingB.append(name)
+    if name[0] == "C":
+        buildingC.append(name)
+    else:
+        print "someting strange afoot in",name 
+
+for building in [buildingA, buildingB, buildingC]:
     ##get an outputfile ready
-    outputFileName = '\checkingList_'+building+'.html'
+    outputFileName = '\checkingList_'+building[0][0]+'.html'
     open(path + outputFileName, 'w').close()
     
     f = open(path + outputFileName, 'a')
@@ -67,30 +91,65 @@ for building in ["A", "B", "C"]:
         variable = 'Visualising solar access to these faces',
         when = datetime.date.today() ) )
     
-    #a= [( x.building_level, x.appartment, x.appartment_window) for x in saImages ]
-    #print a
-    
     #build out a square array of rows
     rows=[]
-    #for month in range(5,7):
     month = 6
-    for level in range(0,12):
-        for appt in range(10):
-            for win in range(5):
-                #print "building",building,"level", level, "appt", appt, "win", win              
-                r = [x for x in saImages if x.building == building and x.building_level == level and x.appartment == appt and x.appartment_window == win]
-                r = sortOnMultipleKeys(r, ('hour','minute'))   
-                #print r             
-                rows.append(r)            
+    for apartment in building:        
+        apptDetails = apartment.split("-")
+        bld   = apptDetails[0] #a letter; A, B or C
+        level = int(apptDetails[1], base=10 )
+        appt  = int(apptDetails[2], base=10 )
+        win   = int(apptDetails[3], base=10 )
+        
+        
+        r = [x for x in saImages if x.name == apartment]
+        #x.building == bld and x.building_level == level and x.appartment == appt and x.appartment_window == win]
+        
+        #print  apartment, len(r)
+
+        r = sortOnMultipleKeys(r, ('hour','minute'))  
+        #if len(r)>0: 
+        #    print "building",building[0][0],"level", level, "appt", appt, "win", win 
+        #    print len(r)#, r[0]
+        rows.append(r)            
+    
+    example = { "ID": 90, "min": 45, "hour": 19, "month":  6, "path": "C:\\Users\\bdoherty\\Desktop\\AMP_solar", 
+                "name": "C-11-6-1", "filename": "faceID_90_name_C_11_6_1_month_6_hr_19_min_45_wpc_0.0.png", 
+                "building_level": 6, "appartment": "6", "appartment_window": 1, "pcWhite": 0.0, "building": "C" }
+
+    def sortcriteria(aRow):
+        #print aRow
+        x = aRow[0]
+        #print x
+        sortString = str(x.building_level)+str(x.appartment)+str(x.appartment_window)
+        #print sortString
+        sorter =int(sortString, base=10) 
+        print sorter, type(sorter)
+        return sorter
+    
+    rowsWithIDs=[]
+    for r in rows:
+        rowsWithIDs.append({"level":r[0].building_level,"appt":r[0].appartment,"win":r[0].appartment_window,"row":r})
+    
+    #sortcriteria(rows[0])
+    rows = sorted(rowsWithIDs, key=lambda x:x["win"])
+    rows = sorted(rowsWithIDs, key=lambda x:x["appt"])
+    rows = sorted(rowsWithIDs, key=lambda x:x["level"])
+    #sortOnMultipleKeys(rowsWithIDs,("win","appt","level"))
+    
+    hardPasses=0
+    softPasses=0
+    nonZero   =0
+    fail      =0
     
     for r in rows:
+        r = r["row"]
         if len(r) != 0:
             # count the number of images in each row (one row per window plane) and 
             # put that value in -timeWithAnyLight-, then recount with stricter criteria
             # and put that value into the -timeInBracketWithLight- variable.
             # then translate these numbers into types of pass.
-            
-            
+                        
             timeWithAnyLight       = len([img for img in r if  img.pcWhite > 0 ])/numberOfImagesPerHour
             timeInBracketWithLight = len([img for img in r if  img.pcWhite > 0 
                                                             and img.hour >= bracketStartTime 
@@ -98,24 +157,32 @@ for building in ["A", "B", "C"]:
             passStatus = 'unknown'
             if timeInBracketWithLight > numberOfHoursToPass:
                 passStatus = "hard-pass"
+                hardPasses+=1
             elif timeWithAnyLight     > numberOfHoursToPass:
                 passStatus = "soft-pass"
+                softPasses+=1
             elif timeWithAnyLight     > 0:
                 passStatus = "non-zero"
+                nonZero+=1
             elif timeWithAnyLight    == 0:
                 passStatus = 'fail'
+                fail+=1
             
             window={
                 'totalHours'    : timeWithAnyLight,
                 'inBracketHours': timeInBracketWithLight,
                 'passStatus'    : passStatus
             }
-            print window
+            #print window
             f.write( msat.eachimgTempl.render(row=r, monthNames=months, window=window) )
     
-    f.write( msat.tailTmpl.render(
-        variable = 'Visualising solar access to these faces'
-        ))
+    total = hardPasses + softPasses + nonZero + fail
+    resultString =("hardPasses: "+str(hardPasses) + " - " + str(hardPasses/float(total)*100) + "%<br>" +
+                   "softPasses: "+str(softPasses) + " - " + str(softPasses/float(total)*100) + "%<br>" +
+                   "nonZero   : "+str(nonZero)    + " - " + str(nonZero/float(total)*100) + "%<br>" +
+                   "fail      : "+str(fail)       + " - " + str(fail/float(total)*100) + "%<br>")
+    print "\n".join(resultString.split('<br>'))
+    f.write( msat.tailTmpl.render( variable = resultString))
         
     f.close()
 
